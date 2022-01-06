ee8fc33acd94e1035ca0d1850deadc1725a7a60f
Maksim Maksimov <mmax77@mail.ru>
2022-01-06 14:29:13 +0500

    shet2

--- trash.py
+++ trash.py
@@ -2,8 +2,4 @@ print('TrashTrashTrashTrashTrashTrashTrashTrashTrashTrash')
 print('CleanCleanCleanCleanCleanCleanCleanCleanCleanClean')
 print('CleanCleanCleanCleanCleanCleanCleanCleanCleanClean')
 print('TrashTrashTrashTrashTrashTrashTrashTrashTrashTrash')
-<<<<<<< HEAD
-print('CleanCleanCleanCleanCleanCleanCleanCleanCleanClean')
-print('TrashTrashTrashTrashTrashTrashTrashTrashTrashTrash')
-=======
->>>>>>> parent of d9818d1 (del 2 string)
+
